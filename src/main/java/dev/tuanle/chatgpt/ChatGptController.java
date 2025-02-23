1ed3a1d68bc9da70e72fa330114c59ef5d852e2f
le nha tuan <lenhatuan2404@gmail.com>
2025-02-23 21:23:41 +0700

    Deploy render

--- src/main/java/dev/tuanle/chatgpt/ChatGptController.java
+++ src/main/java/dev/tuanle/chatgpt/ChatGptController.java
@@ -3,8 +3,6 @@ package dev.tuanle.chatgpt;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
